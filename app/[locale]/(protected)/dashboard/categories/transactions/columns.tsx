fb470adacbeae4c54e2dd30bbb538970912fe36d
MinaSeiffen <minaseiffen@gmail.com>
2025-04-21 18:07:30 +0200

    handling category page

--- app/[locale]/(protected)/dashboard/categories/transactions/columns.tsx
+++ app/[locale]/(protected)/dashboard/categories/transactions/columns.tsx
@@ -1,11 +1,7 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { SquarePen, Trash2 } from "lucide-react";
-import { Checkbox } from "@/components/ui/checkbox";
 import { Link } from '@/i18n/routing';
-import Image from "next/image";
 import {usePathname} from "next/navigation";
-import {Badge} from "@/components/ui/badge";
-import {cn} from "@/lib/utils";
 
 export type DataProps = {
   id: string | number;
@@ -58,20 +54,20 @@ export const columns: ColumnDef<DataProps>[] = [
         if (pathname?.includes('/categories')) {
           return '/dashboard/edit-category';
         } else {
-          return '/utility/invoice/preview/1';
+          return '/dashboard/edit-category';
         }
       };
       return (
         <div className="flex items-center gap-1">
           <Link
             href={getHref()}
-            className="flex items-center p-2 border-b text-info hover:text-info-foreground bg-info/40 hover:bg-info duration-200 transition-all rounded-full"
+            className="flex items-center p-2 border-b text-info hover:text-info-foreground bg-info/40 hover:bg-info duration-200 transition-all rounded-full cursor-pointer"
           >
             <SquarePen className="w-4 h-4" />
           </Link>
           <div
               onClick={() => console.log('delete')}
-            className="flex items-center p-2 text-destructive bg-destructive/40 duration-200 transition-all hover:bg-destructive/80 hover:text-destructive-foreground rounded-full"
+            className="flex items-center p-2 text-destructive bg-destructive/40 duration-200 transition-all hover:bg-destructive/80 hover:text-destructive-foreground rounded-full cursor-pointer"
           >
             <Trash2 className="w-4 h-4" />
           </div>
