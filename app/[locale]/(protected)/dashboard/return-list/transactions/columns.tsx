219ef46e5fc32dd67161445a38ffba4eb506c48a
MinaSeiffen <minaseiffen@gmail.com>
2025-05-01 10:55:45 +0300

    return details page almost done

--- app/[locale]/(protected)/dashboard/return-list/transactions/columns.tsx
+++ app/[locale]/(protected)/dashboard/return-list/transactions/columns.tsx
@@ -1,14 +1,10 @@
 import { ColumnDef } from "@tanstack/react-table";
-// import {
-//   Download,
-//   Eye,
-//   Trash2,
-// } from "lucide-react";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { cn } from "@/lib/utils";
-// import { Link } from '@/i18n/routing';
+import {Download, Eye, Trash2} from "lucide-react";
+import {Link} from "@/i18n/routing";
 
 export type DataProps = {
   id: string | number;
@@ -30,33 +26,6 @@ export type DataProps = {
   action: React.ReactNode;
 };
 export const columns: ColumnDef<DataProps>[] = [
-  {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-        className="bg-default-100"
-      />
-    ),
-    cell: ({ row }) => (
-      <div className="xl:w-16">
-        <Checkbox
-          checked={row.getIsSelected()}
-          onCheckedChange={(value) => row.toggleSelected(!!value)}
-          aria-label="Select row"
-          className="bg-default-100"
-        />
-      </div>
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
-
   {
     accessorKey:"name",
     header: "Inventory Name",
@@ -136,34 +105,22 @@ export const columns: ColumnDef<DataProps>[] = [
       );
     },
   },
-  // {
-  //   id: "actions",
-  //   accessorKey: "action",
-  //   header: "Actions",
-  //   enableHiding: false,
-  //   cell: ({ row }) => {
-  //     return (
-  //       <div className="flex items-center gap-1">
-  //         <Link
-  //           href="/utility/invoice/preview/1"
-  //           className="flex items-center p-2 border-b text-warning hover:text-warning-foreground bg-warning/20 hover:bg-warning duration-200 transition-all rounded-full"
-  //         >
-  //           <Eye className="w-4 h-4" />
-  //         </Link>
-  //         <Link
-  //           href="#"
-  //           className="flex items-center p-2 border-b text-info hover:text-info-foreground bg-info/20 hover:bg-info duration-200 transition-all rounded-full"
-  //         >
-  //           <Download className="w-4 h-4" />
-  //         </Link>
-  //         <Link
-  //           href="#"
-  //           className="flex items-center p-2 text-destructive bg-destructive/40 duration-200 transition-all hover:bg-destructive/80 hover:text-destructive-foreground rounded-full"
-  //         >
-  //           <Trash2 className="w-4 h-4" />
-  //         </Link>
-  //       </div>
-  //     );
-  //   },
-  // },
+  {
+    id: "actions",
+    accessorKey: "action",
+    header: "Actions",
+    enableHiding: false,
+    cell: ({ row }) => {
+      return (
+        <div className="flex items-center gap-1">
+          <Link
+            href={`/dashboard/return-details/${row.original.id}`}
+            className="flex items-center p-2 border-b text-warning hover:text-warning-foreground bg-warning/20 hover:bg-warning duration-200 transition-all rounded-full"
+          >
+            <Eye className="w-4 h-4" />
+          </Link>
+        </div>
+      );
+    },
+  },
 ];
