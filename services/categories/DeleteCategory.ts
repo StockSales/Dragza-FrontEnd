4fda1a76fa4b714a5a811e8d66847e05a3bc1ec3
Shrouk Mostafa Ahmed <shroukmostafa0075@gmail.com>
2025-05-05 16:21:46 +0200

    DeleteCategory.ts

--- services/categories/DeleteCategory.ts
+++ services/categories/DeleteCategory.ts
@@ -1,1 +1,34 @@
+import { useState } from "react";
+import AxiosInstance from "@/lib/AxiosInstance";
+
+function useDeleteCategoryById() {
+    const [loading, setLoading] = useState(false);
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+
+    const deleteCategoryById = async (id: string | string[] | undefined) => {
+        setLoading(true);
+        setError(null);
+        setSuccess(false);
+
+        try {
+            const response = await AxiosInstance.delete(`/api/Categories/${id}`);
+
+            if (response.status === 200) {
+                setSuccess(true);
+            } else {
+                throw new Error("Something went wrong");
+            }
+        } catch (err: any) {
+            setError(err.response?.data?.message || err.message);
+            throw new Error(err.response?.data?.message || err.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return { deleteCategoryById, loading, success, error };
+}
+
+export default useDeleteCategoryById;
 
