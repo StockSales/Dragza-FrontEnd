4569602b1767c383bb2cf0f09b3050c4ef4b07b9
MinaSeiffen <minaseiffen@gmail.com>
2025-06-14 19:26:22 +0300

    Invoice Orders are done

---
+++ app/[locale]/(protected)/dashboard/reports/page.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Button} from "@/components/ui/button";
+import {Link} from "@/i18n/routing";
+
+function Reports() {
+    return (
+        <>
+            <h1 className="text-2xl font-bold mb-4">Reports</h1>
+            <hr className={"bg-blue-100 w-full"}/>
+            <div className="grid grid-cols-3 gap-4 p-4">
+                <Link
+                    href={"/dashboard/reports/orders"}
+                    className="w-full"
+                >
+                    <Button
+                        size={"lg"}
+                        variant={"outline"}
+                        className="w-full"
+                    >
+                        Orders
+                    </Button>
+                </Link>
+                <Link
+                    href={"/dashboard/reports/invoices"}
+                    className="w-full"
+                >
+                    <Button size={"lg"} variant={"outline"} className="w-full">
+                        Invoices
+                    </Button>
+                </Link>
+            </div>
+        </>
+    );
+}
+
+export default Reports;
