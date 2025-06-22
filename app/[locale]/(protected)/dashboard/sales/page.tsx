76ed43aa0c9fcda648ff98db2f2660ab7e270e67
MinaSeiffen <minaseiffen@gmail.com>
2025-06-22 14:18:17 +0300

    sales page is done waiting for approve

--- app/[locale]/(protected)/dashboard/sales/page.tsx
+++ app/[locale]/(protected)/dashboard/sales/page.tsx
@@ -1,49 +1,78 @@
-import React from 'react';
-import {Card, CardContent, CardHeader} from "@/components/ui/card";
-import ColumnNegativeValues
-    from "@/app/[locale]/(protected)/charts/appex-charts/charts-appex-column/column-negative-values";
+"use client";
+
+import React from "react";
+import {
+    Tabs,
+    TabsList,
+    TabsTrigger,
+    TabsContent
+} from "@/components/ui/tabs";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+
+import ColumnNegativeValues from "@/app/[locale]/(protected)/charts/appex-charts/charts-appex-column/column-negative-values";
 import DistributedChart from "@/app/[locale]/(protected)/charts/appex-charts/charts-appex-column/distributed-chart";
 import BasicBar from "@/app/[locale]/(protected)/charts/appex-charts/charts-appex-bar/basic-bar";
 import BasicDonut from "@/app/[locale]/(protected)/charts/appex-charts/charts-appex-pie/basic-donut";
+import TransactionsTable from "@/app/[locale]/(protected)/dashboard/sales/transactions";
 
 function Sales() {
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-3">
-            <Card>
-                <CardHeader>
-                    Sales Statistics
-                </CardHeader>
-                <CardContent>
-                    <ColumnNegativeValues/>
-                </CardContent>
+        <Tabs defaultValue="area-statistics" className="w-full space-y-4 bg-white dark:bg-gray-800 p-4 rounded-lg shadow-md">
+            <TabsList className="grid w-full grid-cols-2 md:grid-cols-4">
+                <TabsTrigger className={"cursor-pointer hover:text-white hover:bg-gray-300 rounded-md"} value="area-statistics"
+                >
+                    Area Statistics
+                </TabsTrigger>
+                <TabsTrigger className={"cursor-pointer hover:text-white hover:bg-gray-300 rounded-md"} value="pharmacy-statistics"
+                >
+                    Pharmacies Statistics
+                </TabsTrigger>
+                <TabsTrigger className={"cursor-pointer hover:text-white hover:bg-gray-300 rounded-md"} value="inventory-statistics"
+                >
+                    Inventory statistics
+                </TabsTrigger>
+                <TabsTrigger className={"cursor-pointer hover:text-white hover:bg-gray-300 rounded-md"} value="status-statistics"
+                >
+                    Status statistics
+                </TabsTrigger>
+            </TabsList>
 
-            </Card>
+            <TabsContent value="area-statistics">
+                <Card>
+                    <CardHeader>Area Statistics</CardHeader>
+                    <CardContent>
+                        <TransactionsTable type={"area"} />
+                    </CardContent>
+                </Card>
+            </TabsContent>
 
-            <Card>
-                <CardHeader>
-                    Sales Representative
-                </CardHeader>
-                <CardContent>
-                    <DistributedChart/>
-                </CardContent>
-            </Card>
-            <Card>
-                <CardHeader>
-                    Best Selling
-                </CardHeader>
-                <CardContent>
-                    <BasicBar/>
-                </CardContent>
-            </Card>
-            <Card>
-                <CardHeader>
-                    Highest return rate
-                </CardHeader>
-                <CardContent>
-                    <BasicDonut/>
-                </CardContent>
-            </Card>
-        </div>
+            <TabsContent value="pharmacy-statistics">
+                <Card>
+                    <CardHeader>Pharmacies Statistics</CardHeader>
+                    <CardContent>
+                        <TransactionsTable type={"pharmacy"} />
+                    </CardContent>
+                </Card>
+            </TabsContent>
+
+            <TabsContent value="inventory-statistics">
+                <Card>
+                    <CardHeader>Inventory statistics</CardHeader>
+                    <CardContent>
+                        <TransactionsTable type={"inventory"} />
+                    </CardContent>
+                </Card>
+            </TabsContent>
+
+            <TabsContent value="status-statistics">
+                <Card>
+                    <CardHeader>Status statistics</CardHeader>
+                    <CardContent>
+                        <TransactionsTable type={"status"} />
+                    </CardContent>
+                </Card>
+            </TabsContent>
+        </Tabs>
     );
 }
 
