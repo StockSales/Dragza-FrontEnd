45e12bbd086cd83c93c206aaa84c7b151f2418c9
hasanMohamed99 <hasan.mohamed.dev@gmail.com>
2025-10-02 14:57:41 +0300

    updated the product-list

--- services/products/gettingAllProducts.ts
+++ services/products/gettingAllProducts.ts
@@ -1,49 +1,53 @@
-import {useState} from 'react';
-import {ProductType} from "@/types/product";
+import { useState } from "react";
+import { ProductType } from "@/types/product";
 import AxiosInstance from "@/lib/AxiosInstance";
 
 function useGettingAllProducts() {
-    const [loading, setLoading] = useState<boolean>(false);
-    const [products, setProducts] = useState<ProductType[]>([]);
-    const [error, setError] = useState<string | null>(null);
-    const [includeDeletedState, setIncludeDeletedState] = useState<string>("false");
+  const [loading, setLoading] = useState<boolean>(false);
+  const [products, setProducts] = useState<ProductType[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [includeDeletedState, setIncludeDeletedState] =
+    useState<string>("false");
 
+  const getAllProducts = async (includeDeleted: string) => {
+    setLoading(true);
+    setError(null);
+    setIncludeDeletedState(includeDeleted);
+    await AxiosInstance.get(
+      `/api/Products?includeDeleted=${includeDeleted}&lang=3`
+    )
+      .then((res) => {
+        if (res.status === 200 || res.status === 201 || !res.data.errors) {
+          setProducts(res.data.data);
+        } else {
+          const firstKey = Object.keys(res.data.errors)[0];
+          const firstMessage = res.data.errors[firstKey][0];
+          setError(`${firstKey}: ${firstMessage}`);
+        }
+      })
+      .catch((err) => {
+        const apiErrors = err?.response?.data?.errors;
+        if (apiErrors && typeof apiErrors === "object") {
+          const firstKey = Object.keys(apiErrors)[0];
+          const firstMessage = apiErrors[firstKey][0];
+          setError(`${firstKey}: ${firstMessage}`);
+        } else {
+          setError("An unexpected error occurred.");
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
 
-    const getAllProducts = async (includeDeleted: string) => {
-        setLoading(true);
-        setError(null);
-        setIncludeDeletedState(includeDeleted);
-        await AxiosInstance.get(`/api/Products?includeDeleted=${includeDeleted}&lang=3`).then((res) => {
-            if (res.status === 200 || res.status === 201 || !res.data.errors) {
-                setProducts(res.data);
-            } else {
-                const firstKey = Object.keys(res.data.errors)[0];
-                const firstMessage = res.data.errors[firstKey][0];
-                setError(`${firstKey}: ${firstMessage}`);
-            }
-        }).catch((err) => {
-            const apiErrors = err?.response?.data?.errors;
-            if (apiErrors && typeof apiErrors === "object") {
-                const firstKey = Object.keys(apiErrors)[0];
-                const firstMessage = apiErrors[firstKey][0];
-                setError(`${firstKey}: ${firstMessage}`);
-            } else {
-                setError("An unexpected error occurred.");
-            }
-        }).finally(() => {
-            setLoading(false);
-        })
-    }
-
-    return {
-        getAllProducts,
-        loading,
-        error,
-        products,
-        includeDeleted: includeDeletedState,
-        setIncludeDeletedState
-    }
-
+  return {
+    getAllProducts,
+    loading,
+    error,
+    products,
+    includeDeleted: includeDeletedState,
+    setIncludeDeletedState,
+  };
 }
 
 export default useGettingAllProducts;
