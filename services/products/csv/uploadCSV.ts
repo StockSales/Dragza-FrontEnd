567826b6bdffaebab6c129d32d9106c20011fd65
omarabozied5 <omarabozeid.pro@gmail.com>
2025-10-13 20:14:48 +0300

    update csv uplaod for invetory managers

--- services/products/csv/uploadCSV.ts
+++ services/products/csv/uploadCSV.ts
@@ -1,32 +1,38 @@
-import {useState} from "react";
+import { useState } from "react";
 import AxiosInstance from "@/lib/AxiosInstance";
 
 function useUploadCsv() {
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
-    const uploadCSV = async (file: File) => {
-        setLoading(true);
-        setError(null);
-        try {
-            const formData = new FormData();
-            formData.append('file', file);
-            const response = await AxiosInstance.post('/api/Products/import-excel', formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            });
-            if (response.status !== 200) {
-                throw new Error('Failed to upload file');
-            }
-        } catch (error) {
-            setError(error instanceof Error ? error.message : 'Failed to upload file');
-        } finally {
-            setLoading(false);
+  const uploadCSV = async (file: File) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+      const response = await AxiosInstance.post(
+        "/api/Products/import-product-prices-excel",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
         }
-    };
+      );
+      if (response.status !== 200) {
+        throw new Error("Failed to upload file");
+      }
+    } catch (error) {
+      setError(
+        error instanceof Error ? error.message : "Failed to upload file"
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return {loading, error, uploadCSV};
+  return { loading, error, uploadCSV };
 }
 
 export default useUploadCsv;
